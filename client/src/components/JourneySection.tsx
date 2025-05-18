9758ecee31068c59a8c7d8ea47f338b6f4157210
Nigel Ong <nigel.ong00@gmail.com>
2025-05-18 00:57:12 -0700

    removed the image from bcit campus and centered the funfact again

--- client/src/components/JourneySection.tsx
+++ client/src/components/JourneySection.tsx
@@ -131,7 +131,8 @@ const JourneySection = () => {
               >
                 <div className="grid md:grid-cols-2 gap-4 items-start">
                   <img
-                    src="https://pixabay.com/get/gef52cd873e4315992c735f4269b5550b92aa15ffeeb4d6f5a2d5a4212d986772bcc2f348e0b548023e763f18d926fc6c4042b2b57bf3d94654781823019c078b_1280.jpg"
+                    src=""
+                    // src="https://pixabay.com/get/gef52cd873e4315992c735f4269b5550b92aa15ffeeb4d6f5a2d5a4212d986772bcc2f348e0b548023e763f18d926fc6c4042b2b57bf3d94654781823019c078b_1280.jpg"
                     alt="BCIT Campus"
                     className="w-full h-auto rounded-lg shadow-md"
                     loading="lazy"
